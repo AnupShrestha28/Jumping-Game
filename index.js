6ba3bb5d23f6f57e68af76cfcf5a003e7c72683e
AnupShrestha28 <anupstha0168@gmail.com>
2022-11-18 13:21:46 +0545

    working

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ canvas.height = 576
 
 const scaledCanvas = {
     width: canvas.width / 4,
-    height: canvas.height / 4
+    height: canvas.height / 4,
 }
 
 const floorCollisions2D = []
@@ -14,65 +14,51 @@ for (let i = 0; i < floorCollisions.length; i += 36) {
     floorCollisions2D.push(floorCollisions.slice(i, i + 36))
 }
 
-floorCollisions2D.forEach((row) => {
-    row.forEach((symbol) => {
+const collisionBlocks = []
+floorCollisions2D.forEach((row, y) => {
+    row.forEach((symbol, x) => {
         if (symbol === 202) {
-            console.log('draw a block here');
+            collisionBlocks.push(
+                new CollisionBlock({
+                    position: {
+                        x: x * 16,
+                        y: y * 16,
+                    },
+                })
+            )
+        }
+    })
+})
+
+const platformCollisions2D = []
+for (let i = 0; i < platformCollisions.length; i += 36) {
+    platformCollisions2D.push(platformCollisions.slice(i, i + 36))
+}
+
+const platformCollisionBlocks = []
+platformCollisions2D.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        if (symbol === 202) {
+            platformCollisionBlocks.push(
+                new CollisionBlock({
+                    position: {
+                        x: x * 16,
+                        y: y * 16,
+                    },
+                })
+            )
         }
     })
 })
 
 const gravity = 0.5
 
-class Sprite {
-    constructor({ position, imageSrc }) {
-        this.position = position
-        this.image = new Image()
-        this.image.src = imageSrc
-    }
-
-    draw() {
-        if (!this.image) return
-        c.drawImage(this.image, this.position.x, this.position.y)
-    }
-
-    update() {
-        this.draw()
-    }
-}
-
-class Player {
-    constructor(position) {
-        this.position = position
-        this.velocity = {
-            x: 0,
-            y: 1
-        }
-        this.height = 100
-    }
-
-    draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 100, this.height)
-    }
-
-    update() {
-        this.draw()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        if (this.position.y + this.height + this.velocity.y < canvas.height) this.velocity.y += gravity
-        else this.velocity.y = 0
-    }
-}
-
 const player = new Player({
-    x: 0,
-    y: 0,
-})
-
-const player2 = new Player({
-    x: 300,
-    y: 100,
+    position: {
+        x: 100,
+        y: 0,
+    },
+    collisionBlocks,
 })
 
 const keys = {
@@ -102,15 +88,21 @@ function animate() {
     c.scale(4, 4)
     c.translate(0, -background.image.height + scaledCanvas.height)
     background.update()
-    c.restore()
+    collisionBlocks.forEach((collisionBlock) => {
+        collisionBlock.update()
+    })
+
+    platformCollisionBlocks.forEach((block) => {
+        block.update()
+    })
 
     player.update()
-    player2.update()
-
 
     player.velocity.x = 0
     if (keys.d.pressed) player.velocity.x = 5
     else if (keys.a.pressed) player.velocity.x = -5
+
+    c.restore()
 }
 
 animate()
